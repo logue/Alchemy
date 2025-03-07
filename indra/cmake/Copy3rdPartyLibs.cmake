06f49dc7ace1363178eee123c9b06e43916509ae
Rye <rye@alchemyviewer.org>
2025-03-07 12:34:05 -0500

    Output status feedback while installing autobuild packages

--- indra/cmake/Copy3rdPartyLibs.cmake
+++ indra/cmake/Copy3rdPartyLibs.cmake
@@ -200,16 +200,6 @@ elseif(LINUX)
 
      if( USE_AUTOBUILD_3P )
          list( APPEND release_files
-                 libatk-1.0.so
-                 libfreetype.so.6.6.2
-                 libfreetype.so.6
-                 libopenjp2.so
-                 libuuid.so.16
-                 libuuid.so.16.0.22
-                 libfontconfig.so.1.8.0
-                 libfontconfig.so.1
-                 libgmodule-2.0.so
-                 libgobject-2.0.so
                  )
      endif()
 
