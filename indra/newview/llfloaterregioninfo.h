f308d9cba4e95af23d37f5aba70365a5f5de7271
Rye <rye@alchemyviewer.org>
2025-03-12 02:26:28 -0400

    Fix raw terrain upload/download crashing under modeless-only filepicker platforms

--- indra/newview/llfloaterregioninfo.h
+++ indra/newview/llfloaterregioninfo.h
@@ -267,7 +267,9 @@ public:
     void updateForMaterialType();
 
     static void onClickDownloadRaw(void*);
+    static void onClickDownloadRawCallback(const std::vector<std::string>& filenames, void* data);
     static void onClickUploadRaw(void*);
+    static void onClickUploadRawCallback(void* data, const std::vector<std::string>& filenames);
     static void onClickBakeTerrain(void*);
     bool callbackBakeTerrain(const LLSD& notification, const LLSD& response);
     bool callbackTextureHeights(const LLSD& notification, const LLSD& response);
